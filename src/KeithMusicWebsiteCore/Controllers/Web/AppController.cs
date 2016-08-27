d911181d8732d0bf7aca49593b03f5548393b059
William Sanders <william.sanders.dev@gmail.com>
2016-08-27 15:37:42 -0400

    initial

---
+++ src/KeithMusicWebsiteCore/Controllers/Web/AppController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeithMusicWebsiteCore.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
