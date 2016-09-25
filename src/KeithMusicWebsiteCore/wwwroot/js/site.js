236e1597fe39fd45d6363b7667483d3ae3ed6be0
William Sanders <william.sanders.dev@gmail.com>
2016-09-25 19:29:02 -0400

    improved layout

--- src/KeithMusicWebsiteCore/wwwroot/js/site.js
+++ src/KeithMusicWebsiteCore/wwwroot/js/site.js
@@ -18,23 +18,23 @@ $(window).on("resize", function () {
 
 /************   SETS LOCATION OF NAVIGATION ACCORDING TO SCREEN SIZE   *******************/
 
-var $navigationSelectorsContainer = $("<div class='navigation-selectors-container'></div>")
+//var $navigationSelectorsContainer = $("<div class='navigation-selectors-container'></div>")
 
-var $homeSelector = $("<li class='navigation-selector'><a href='#'>Home</a></li>");
-var $musicSelector = $("<li class='navigation-selector'><a href='#'>Music</a></li>");
-var $aboutSelector = $("<li class='navigation-selector'><a href='#'>About</a></li>");
+//var $homeSelector = $("<li class='navigation-selector'><a href='#'>Home</a></li>");
+//var $musicSelector = $("<li class='navigation-selector'><a href='#'>Music</a></li>");
+//var $aboutSelector = $("<li class='navigation-selector'><a href='#'>About</a></li>");
 
-$navigationSelectorsContainer.append($homeSelector);
-$navigationSelectorsContainer.append($musicSelector);
-$navigationSelectorsContainer.append($aboutSelector);
+//$navigationSelectorsContainer.append($homeSelector);
+//$navigationSelectorsContainer.append($musicSelector);
+//$navigationSelectorsContainer.append($aboutSelector);
 
-$(window).on("resize", function () {
-    if ($("#keith-layout-picture").css("width") == "300px") {
-        $("#keith-layout-picture").after($navigationSelectorsContainer);
-    } else {
-        $("#top-banner").after($navigationSelectorsContainer);
-    }
-}).resize();
+//$(window).on("resize", function () {
+//    if ($("#keith-layout-picture").css("width") == "300px") {
+//        $("#keith-layout-picture").after($navigationSelectorsContainer);
+//    } else {
+//        $("#top-banner").after($navigationSelectorsContainer);
+//    }
+//}).resize();
 
 
 
